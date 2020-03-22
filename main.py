845fdd22ec17594707628ad2cc946ec2c9253ad0
paulosestini <24728744+paulosestini@users.noreply.github.com>
2020-03-22 19:34:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
-
 from Perceptron import Perceptron
-x = np.array([1, 2])
 
+# Creating a 2 dimensional perceptron
 perceptron = Perceptron(2)
 
+# Creating artificial dataset
 data_class_0 = np.random.rand(100, 2)*1.1 + np.array([-1, 1])
 data_labels_0 = np.array([0]*100)
 
@@ -17,15 +17,17 @@ data = np.concatenate((data_class_0, data_class_1))
 labels = np.concatenate((data_labels_0, data_labels_1))
 data, labels = shuffle(data, labels)
 
+# Training the perceptron
 perceptron.train(data, labels)
 
+# Getting the boundary line made by the perceptron
 b = perceptron.w[0]
 w1 = perceptron.w[1]
 w2 = perceptron.w[2]
-
 line_x0, line_x1 = -2, 2
 line_y0, line_y1 = (-b-w1*line_x0)/w2, (-b-w1*line_x1)/w2
 
+# Plotting the data points and the boundary line
 plt.scatter(data[:, 0], data[:, 1])
 plt.plot([line_x0, line_x1], [line_y0, line_y1])
 plt.show()
